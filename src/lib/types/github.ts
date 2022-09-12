2f5c5e4e278d1683b321242b460aa5aa95c441a4
unknown <00ljw00@naver.com>
2022-09-12 14:45:08 +0900

    feat: issue api 및 react query hook 작성
    - axios issue 함수로 저장되어있는 레포의 모든 이슈들 get
    - react query issue hook을 활용하여 select 페이지네이션 데이터 재구성

--- src/lib/types/github.ts
+++ src/lib/types/github.ts
@@ -15,3 +15,21 @@ export type UserType = {
   html_url: string;
   avatar_url: string;
 };
+export type IssueStateType = 'open' | 'closed';
+export type IssueType = {
+  id: number;
+  html_url: string;
+  number: number;
+  title: string;
+  user: UserType;
+  state: IssueStateType;
+  comments: number;
+  updated_at: Date;
+  body: string;
+};
+export type IssuePaginationType = {
+  issueList: IssueType[];
+  totalPage: number;
+  page: number;
+  size: number;
+};
